f0eed19cbfaee83fc3254741fa9a895f03eae343
shaojijie <2858207363@qq.com>
2022-07-24 22:39:48 +0800

    first activity

--- demo/app/src/main/java/com/example/demo/MainActivity.java
+++ demo/app/src/main/java/com/example/demo/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.demo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textView = findViewById(R.id.hello_title);
+        textView.setText("这是我第一个Android项目");
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this,test_MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ demo/app/src/main/java/com/example/demo/test_MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class test_MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test_layouy_main);
+    }
+}
